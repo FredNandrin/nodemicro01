f728a85f4e06724027426c6f893b6342b138a861
Fred <fred@nanit.be>
2021-04-20 22:12:25 +0200

    test

--- src/api/routes.js
+++ src/api/routes.js
@@ -4,7 +4,9 @@ const controller = require('./controller');
 
 module.exports = function(app) {
     app.route('/')
-        .get(controller.about);
+        .get(controller.about)
+    app.route('/about')
+        .get(controller.about);;
 //    app.route('/distance/:zipcode1/:zipcode2')
 //        .get(controller.getDistance);
 };
